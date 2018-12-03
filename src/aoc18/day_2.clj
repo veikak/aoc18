9f3474c738db5d49f3fd43a9d0a1c0231683dea4
Veikka Kähkönen <insiding@gmail.com>
2018-12-03 18:23:03 +0200

    Complete day 2, part 2

--- src/aoc18/day_2.clj
+++ src/aoc18/day_2.clj
@@ -15,6 +15,22 @@
        (filter #(some (partial = n) %))
        count))
 
+(defn sort-by-subs
+  [end coll]
+  (sort #(compare (subs %1 0 end) (subs %2 0 end)) coll))
+
+(defn matching-indices
+  [a b]
+  (->>
+     (map #(not= (compare %1 %2) 0) a b)
+     (keep-indexed #(when (true? %2) %1))))
+
+(defn diff-by-1
+  [a b]
+  (let [indices (matching-indices a b)]
+    (when (= (count indices) 1)
+      (first indices))))
+
 (defn part-1
   []
   (let
@@ -24,3 +40,18 @@
            (map vals))
     checksum (reduce * 1 (map #(count-appearances % freqs) [2 3]))]
    (println checksum)))
+
+(defn part-2
+  []
+  (let
+   [in (puzzle-input)
+    id-len (count (first in))
+    sorted-by-subs (map #(sort-by-subs %1 in) (range id-len))]
+   (loop
+    [ids (reduce concat () sorted-by-subs)]
+     (let [[first-id & [second-id & rest-ids]] ids]
+       (if-let [diff-i (diff-by-1 first-id second-id)]
+         (println (str
+                   (subs first-id 0 diff-i)
+                   (subs first-id (+ diff-i 1) (count first-id))))
+         (recur rest-ids))))))
