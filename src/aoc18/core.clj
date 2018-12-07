89366dce0a85b85e9796e36228f363fce516d860
Veikka Kähkönen <insiding@gmail.com>
2018-12-07 13:56:24 +0200

    Implement day 5, part 1

--- src/aoc18/core.clj
+++ src/aoc18/core.clj
@@ -4,7 +4,8 @@
              day-1
              day-2
              day-3
-             day-4]))
+             day-4
+             day-5]))
 
 (defn -main
   [& args]
---
+++ src/aoc18/day_5.clj
@@ -0,0 +1,46 @@
+(ns aoc18.day-5
+  (:require [clojure.java.io :as jio])
+  (:require [clojure.string :as string]))
+
+(defn puzzle-input []
+  (->>
+   "day_5.txt"
+   jio/resource
+   slurp
+   string/trim))
+
+(defn zero-clamp [n]
+  (if (< n 0) 0 n))
+
+(defn reactive? [a b]
+  (and (not= a b) (apply = (map #(string/lower-case %) [a b]))))
+
+(defn first-reactive-pair
+  ([polymer]
+   (first-reactive-pair polymer 0))
+  ([[head & rest] i]
+   (when rest
+     (if (reactive? head (first rest))
+       i
+       (recur rest (inc i))))))
+
+(defn react-polymer
+  ([polymer]
+   (react-polymer polymer 0))
+  ([polymer start]
+   (if-let
+    [sub-i (first-reactive-pair (subs polymer start))]
+     (let
+      [global-i (+ start sub-i)
+       post-reaction
+       (str (subs polymer 0 global-i)
+            (subs polymer (+ global-i 2) (count polymer)))]
+       (recur post-reaction (zero-clamp (dec (+ start sub-i)))))
+     polymer)))
+
+(defn part-1 []
+  (->>
+   (puzzle-input)
+   react-polymer
+   count
+   println))
