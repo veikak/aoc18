bf702bc5ad0dc04fc255078895faa25677412eb0
Veikka Kähkönen <insiding@gmail.com>
2018-12-07 13:53:27 +0200

    AOT compile the core

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot aoc18.core
+  :aot [aoc18.core]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
